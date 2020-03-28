5f783525b3bf94ddca55e306b482ebaf97021a69
F-howk <34292531+F-howk@users.noreply.github.com>
2020-03-28 18:21:26 +0800

    Add files via upload

---
+++ js/chunk-2d0d6b9d.7ff85328.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d6b9d"],{"747a":function(t,e,n){"use strict";n.r(e);var r=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{attrs:{id:"goods-details"}},[n("h1",[t._v("商品详情")])])}],s={data:function(){return{}},methods:{}},u=s,c=n("2877"),d=Object(c["a"])(u,r,a,!1,null,null,null);e["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d0d6b9d.7ff85328.js.map
